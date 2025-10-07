cda2180a8af38cac69d82fe8ad8471c2a98aadcc
Iggy Siegel <iggysiegel@gmail.com>
2025-10-07 16:19:48 -0400

    Updated scraper script with NFL weather data

--- scripts/scrape_data.py
+++ scripts/scrape_data.py
@@ -1,9 +1,10 @@
 """Raw Data Scraper: scrape_data.py.
 
-Scrape NFL data from multiple sources (e.g., Pro Football Reference, nfl_data_py)
-and save raw data in the raw data directory.
+Scrape NFL data from multiple sources (e.g., NFLWeather.com, nfl_data_py,
+Pro Football Reference) and save raw data in the raw data directory.
 
 Output:
+    - NFLWeather.com: JSON files
     - nfl_data_py: CSV files
     - Pro Football Reference: JSON files
 
@@ -21,7 +22,9 @@ import json
 import os
 
 import pandas as pd
+
 from src.data.nfl_data_py_scraper import scrape_season as ndp_scrape
+from src.data.nfl_weather_scraper import NflWeatherScraper
 from src.data.pro_football_reference_scraper import scrape_season as pfr_scrape
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -63,8 +66,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for season in range(args.start_season, args.end_season + 1):
+        # NFLWeather.com
+        weather_data = NflWeatherScraper().parse_season(season)
+        save_data(weather_data, "weather", season, RAW_DATA_DIR)
+        print(f"{season} Weather done.", flush=True)
+
+        # nfl_data_py
         ndp_season = ndp_scrape(season)
         save_data(ndp_season, "nfl_data_py", season, RAW_DATA_DIR)
+
+        # Pro Football Reference
         pfr_season = pfr_scrape(season)
         save_data(pfr_season, "pro_football_reference", season, RAW_DATA_DIR)
         print(f"{season} Pro Football Reference done.", flush=True)
