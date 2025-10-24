0959c3ce4fb2ebade3412bc44b1c673cf206bb4b
Iggy Siegel <iggysiegel@gmail.com>
2025-10-24 17:55:27 -0400

    Add prepare_data function to utils.py

---
+++ src/models/utils.py
@@ -0,0 +1,59 @@
+"""Utility functions for machine learning pipline."""
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+def prepare_data(
+    df: pd.DataFrame,
+    partition_season: int,
+    partition_week: int,
+    feature_cols: list[str],
+    target_col: str,
+) -> tuple[
+    pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.Series, pd.Series
+]:
+    """Prepares training and testing datasets from the provided DataFrame.
+
+    Args:
+        df: The input DataFrame containing the data.
+        partition_season: The season to partition the data.
+        partition_week: The week to partition the data.
+        feature_cols: List of column names to be used as features.
+        target_col: The column name to be used as the target variable.
+
+    Returns:
+        A tuple containing:
+        - df_train: pd.DataFrame - Training dataset.
+        - df_test: pd.DataFrame - Testing dataset.
+        - x_train: pd.DataFrame - Training feature set.
+        - x_test: pd.DataFrame - Testing feature set.
+        - y_train: pd.Series - Training target variable.
+        - y_test: pd.Series - Testing target variable.
+    """
+    # Split the data into training and testing sets based on season and week
+    train_df = df[
+        (df["season"] < partition_season)
+        | ((df["season"] == partition_season) & (df["week"] < partition_week))
+    ]
+    test_df = df[
+        (df["season"] > partition_season)
+        | ((df["season"] == partition_season) & (df["week"] >= partition_week))
+    ]
+
+    # Extract features and target variables
+    x_train = train_df[feature_cols]
+    y_train = train_df[target_col]
+    x_test = test_df[feature_cols]
+    y_test = test_df[target_col]
+
+    # Standardize the feature sets
+    scaler = StandardScaler().fit(x_train)
+    x_train = pd.DataFrame(
+        scaler.transform(x_train), columns=feature_cols, index=x_train.index
+    )
+    x_test = pd.DataFrame(
+        scaler.transform(x_test), columns=feature_cols, index=x_test.index
+    )
+
+    return train_df, test_df, x_train, x_test, y_train, y_test
