ed46a86b3e8730b85e74801c52bd72f383614717
Iggy Siegel <iggysiegel@gmail.com>
2025-10-26 16:43:20 -0400

    Add prediction path

--- src/paths.py
+++ src/paths.py
@@ -5,6 +5,7 @@ from pathlib import Path
 ROOT_DIR = ROOT_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = ROOT_DIR / "data"
 RAW_DIR = DATA_DIR / "raw"
+PREDICTION_DIR = DATA_DIR / "predictions"
 
-for d in [DATA_DIR, RAW_DIR]:
+for d in [DATA_DIR, RAW_DIR, PREDICTION_DIR]:
     d.mkdir(parents=True, exist_ok=True)
