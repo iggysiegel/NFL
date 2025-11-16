e54c035119c43101093e906893ccb18b7efde3f9
Iggy Siegel <iggysiegel@gmail.com>
2025-11-16 11:24:39 -0500

    Add SRC path

--- src/paths.py
+++ src/paths.py
@@ -3,7 +3,8 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
+SRC_DIR = ROOT_DIR / "src"
 PREDICTION_DIR = ROOT_DIR / "predictions"
 
-for d in [PREDICTION_DIR]:
+for d in [SRC_DIR, PREDICTION_DIR]:
     d.mkdir(parents=True, exist_ok=True)
