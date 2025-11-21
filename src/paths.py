ae9676817667d0e66961d0c41368a770e9298a37
Iggy Siegel <iggysiegel@gmail.com>
2025-11-21 14:53:48 -0500

    Add MODEL_DIR

--- src/paths.py
+++ src/paths.py
@@ -5,6 +5,7 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parents[1]
 SRC_DIR = ROOT_DIR / "src"
 PREDICTION_DIR = ROOT_DIR / "predictions"
+MODEL_DIR = ROOT_DIR / "models"
 
-for d in [SRC_DIR, PREDICTION_DIR]:
+for d in [SRC_DIR, PREDICTION_DIR, MODEL_DIR]:
     d.mkdir(parents=True, exist_ok=True)
